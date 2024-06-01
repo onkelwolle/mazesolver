95ac33c6abdb01984cede029b1c7f175da8cdb82
Wolfgang Zimmermann <wolfgang@zimmermann.red>
2024-06-01 15:57:27 +0200

    feat: add draw_move function to cell to draw a line from one cell to another

--- cell.py
+++ cell.py
@@ -7,13 +7,30 @@ class Cell:
         self.has_left_wall = True
         self.has_bottom_wall = True 
         self._win = win
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
 
     def draw(self, _x1, _y1, _x2, _y2, fill_color):
+        self._x1 = _x1
+        self._x2 = _x2
+        self._y1 = _y1
+        self._y2 = _y2
+
         if self.has_left_wall:
-            self._win.draw_line(Line(Point(_x1, _y1), Point(_x1, _y2)), fill_color)
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), fill_color)
         if self.has_bottom_wall:
-            self._win.draw_line(Line(Point(_x1, _y2), Point(_x2, _y2)), fill_color)
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), fill_color)
         if self.has_right_wall:
-            self._win.draw_line(Line(Point(_x2, _y2), Point(_x2, _y1)), fill_color)
+            self._win.draw_line(Line(Point(self._x2, self._y2), Point(self._x2, self._y1)), fill_color)
         if self.has_top_wall:
-            self._win.draw_line(Line(Point(_x1, _y1), Point(_x2, _y1)), fill_color)
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), fill_color)
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        if self._x1 == None or self._x2 == None or self._y1 == None or self._y2 == None:
+            Exception("need to set top left and bottom right corners through draw method")
+        self._win.draw_line(Line(Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2), Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2)), fill_color)
--- main.py
+++ main.py
@@ -4,21 +4,15 @@ from cell import Cell
 def main():
     win = Window(800, 600)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100, "black")
+    c1 = Cell(win)
+    c1.has_bottom_wall = False
+    c1.draw(50, 50, 100, 100, "black")
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200, "black")
+    c2 = Cell(win)
+    c2.has_top_wall = False
+    c2.draw(50, 101, 100, 151, "black")
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250, "black")
-
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500, "black")
+    c1.draw_move(c2)
     win.wait_for_close()
 
 
